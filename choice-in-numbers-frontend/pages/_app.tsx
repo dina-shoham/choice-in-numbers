555e332a745bc66a0876b602cb8017dcf19c3a18
dina <dina.shoham@gmail.com>
2025-05-28 17:44:12 -0400

    cleaning things

--- choice-in-numbers-frontend/pages/_app.tsx
+++ choice-in-numbers-frontend/pages/_app.tsx
@@ -1,35 +1,14 @@
 import { AppProps } from 'next/app';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css'
-import Button from '../components/Button';
-import InputBlack from '../components/InputBlack';
-import InputAccentColour from '../components/InputAccentColour';
-import { useState } from 'react';
 
-export default function MyApp({ Component, pageProps }: AppProps): React.JSX.Element {
-  const [ inputText, setInputText ] = useState('Type here');
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value);
-    if (event.target.value === '') {
-      setInputText('Type here');
-    }
-  };
-
+export default function MyApp({ Component, pageProps }: AppProps): React.JSX.Element {  
   return (
     <>
       <NavBar />
       <main>
         <Component {...pageProps} />
-          <div>
-            <Button ButtonText="Click Me" OnClick={() => console.log("clicked")} />
-          </div>
-          <div>
-            <InputBlack InputText={inputText} OnChange={handleInputChange} Colour='accent-dark' />
-          </div>
-          <div>
-            <InputAccentColour InputText="Type Here" OnChange={() => console.log("typing")} Colour='black' />
-          </div>
       </main>
     </>
   );
--- choice-in-numbers-frontend/pages/index.tsx
+++ choice-in-numbers-frontend/pages/index.tsx
@@ -1,6 +1,46 @@
 import type { NextPage } from 'next';
+import Button from '../components/Button';
+import InputBlack from '../components/InputBlack';
+import InputAccentColour from '../components/InputAccentColour';
+import { useState } from 'react';
+import Select from '../components/Select';
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>;
+
+  const [ inputText, setInputText ] = useState('Type here');
+    const [ selected, setSelected ] = useState('');
+    
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputText(event.target.value);
+      if (event.target.value === '') {
+        setInputText('Type here');
+      }
+    };
+  
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setSelected(event.target.value);
+    };
+  
+
+  return (
+    <div> 
+      <h1>Home</h1>
+        <div>
+          <Button ButtonText="Click Me" OnClick={() => console.log("clicked")} />
+        </div>
+        <div>
+          <InputBlack InputText={inputText} OnChange={handleInputChange} />
+        </div>
+        <div>
+          <InputAccentColour InputText="Type Here" OnChange={() => console.log("typing")} />
+        </div>
+        <div>
+          <Select Placeholder='Select an option'
+                  Options={['Option 1', 'Option 2', 'Option 3']}
+                  OnChange={handleSelectChange}>
+          </Select>
+        </div>
+    </div>
+  );
 };
 export default Home;
